7d206b77f18a1b18b902079d9343de45e74bfd22
MdAfsarHossain <afsar291722@gmail.com>
2025-10-05 16:37:38 +0600

    App module ts file updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,28 @@
+/* eslint-disable prettier/prettier */
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EmployeesController } from './employees/employees.controller';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true
+      synchronize: true,
     }),
     UserModule,
-    EmployeesModule],
+    EmployeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
